6fe5ed6705e2742781001fb659d65cf996f70fed
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 03:23:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 ital_menu = {:lasagna => 12.50, :spaghetti => 12.00, :manicotti => 13.75, :cacio_e_pepe => 11.50}
-def key_for_min_value(ital_menu)
+def key_for_min_value(hash)
   ital_menu.each do |food, price|
     
 end
